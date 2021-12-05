39288a70e7cd64eb11774ba266d588c5a64b19a3
Martun <65818128+martun-avagyan@users.noreply.github.com>
2021-12-05 18:10:44 +0400

    feat: 1. Odd Or Even function

---
+++ oddOrEven.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const oddOrEven = function (arr) {
+  const arrOdd = [];
+  const arrEven = [];
+  const finalArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      arrEven.push(arr[i]);
+    } else {
+      arrOdd.push(arr[i]);
+    }
+  }
+  finalArr.push(arrEven, arrOdd);
+  return finalArr;
+};
+
+console.log(oddOrEven([45, 12, 3, 6, 17, 0]));
